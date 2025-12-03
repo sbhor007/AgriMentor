001ebf340faacf36feb8997a660a81bf05485c16
panjal purkar <purkarpranjal12@gmail.com>
2025-12-03 23:49:27 +0530

    Added feedback and rating module

--- server/src/main/java/com/server/config/SecurityConfig.java
+++ server/src/main/java/com/server/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.server.config;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
